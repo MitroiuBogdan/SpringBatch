78284329ea4def7907a977b7a9f9bcb864928b35
Bogdan Mitroiu <bogdan-constantin.mitroiu@ing.com>
2022-08-07 22:00:06 +0300

    enhance ObjectToExcelWriter - add support for nested headers

--- src/main/java/com/ing/casyadapterpoc/common/utils/ObjectToExcelWriter.java
+++ src/main/java/com/ing/casyadapterpoc/common/utils/ObjectToExcelWriter.java
@@ -15,13 +15,15 @@ import java.io.IOException;
 import java.util.Iterator;
 import java.util.Map;
 
+import static org.apache.logging.log4j.util.Strings.isBlank;
+
 @Slf4j
 public class ObjectToExcelWriter {
 
 
     @SneakyThrows
     public static void writeObjectToExcelFile(Object object, String path) {
-        System.out.println(path);
+        log.info("Path: - {}", path);
         ObjectMapper objectMapper = new ObjectMapper();
         String jsonStr = objectMapper.writeValueAsString(object);
         Workbook workbook;
@@ -40,35 +42,56 @@ public class ObjectToExcelWriter {
 
         if (sheet.getLastRowNum() < 1) {
             Row headerRow = sheet.createRow(0);
-            insertValueIntoCell(jsonNode, RowType.HEADER, headerRow, 0);
+            insertValueIntoCell(jsonNode, null, RowType.HEADER, headerRow, 0);
         }
 
         int rowIndex = sheet.getLastRowNum() + 1;
         Row row = sheet.createRow(rowIndex);
-        insertValueIntoCell(jsonNode, RowType.ROW, row, 0);
+        insertValueIntoCell(jsonNode, null, RowType.ROW, row, 0);
 
         FileOutputStream outputStream = new FileOutputStream(path);
         workbook.write(outputStream);
 
     }
 
-    private static int insertValueIntoCell(JsonNode node, RowType rowType, Row row, int startCellIndex) throws JsonProcessingException {
+    private static int insertValueIntoCell(JsonNode node, String parentNodeName, RowType rowType, Row row, int startCellIndex) throws JsonProcessingException {
         for (Iterator<Map.Entry<String, JsonNode>> it = node.fields(); it.hasNext(); ) {
             Map.Entry<String, JsonNode> field = it.next();
             if (!field.getValue().isValueNode() && !field.getValue().isArray()) {
                 try {
-                    startCellIndex = insertValueIntoCell(field.getValue(), rowType, row, startCellIndex);
+                    startCellIndex = insertValueIntoCell(field.getValue(), field.getKey(), rowType, row, startCellIndex);
                 } catch (JsonProcessingException e) {
                     e.printStackTrace();
                 }
             } else {
-                String cellValue = rowType == RowType.HEADER ? field.getKey() : field.getValue().toString();
-                createCell(row, startCellIndex, cellValue);
-                log.info("index :{} - key: {}, value: {}", startCellIndex, field.getKey(), cellValue);
+                if (RowType.HEADER == rowType) {
+                    String headerValue = constructHeaderValue(parentNodeName, field.getKey());
+                    createCell(row, startCellIndex, headerValue);
+                    log.info("Creating header with - Index :{} - Key: {}, Value: {}", startCellIndex, field.getValue().toString());
+                } else {
+                    String value = !isBlank(field.getValue().textValue()) ? field.getValue().textValue() : "null";
+                    createCell(row, startCellIndex, value);
+                    log.info("Index :{} - Key: {}, Value: {}", startCellIndex, field.getValue());
+                }
                 startCellIndex++;
             }
         }
         return startCellIndex;
+    }
+
+    private static String constructHeaderValue(String parentNodeName, String nodeName) {
+        StringBuilder stringBuilder = new StringBuilder();
+        String headerValue;
+        if (parentNodeName != null) {
+            headerValue = stringBuilder.append(parentNodeName)
+                    .append(".")
+                    .append(nodeName).toString();
+        } else {
+            headerValue = nodeName;
+        }
+
+        log.info("headerValue :{}", headerValue);
+        return headerValue;
     }
 
     //    public static void writeObjectsToExcelFile(List<? extends Object> objList, String path) throws IOException {
--- src/test/java/com/ing/casyadapterpoc/saltedge/rest/ObjectToExcelWriterTest.java
+++ src/test/java/com/ing/casyadapterpoc/saltedge/rest/ObjectToExcelWriterTest.java
@@ -1,6 +1,8 @@
 package com.ing.casyadapterpoc.saltedge.rest;
 
 import com.ing.casyadapterpoc.common.domain.casy_entity.Account;
+import com.ing.casyadapterpoc.common.domain.casy_entity.Merchant;
+import com.ing.casyadapterpoc.common.domain.casy_entity.Transaction;
 import com.ing.casyadapterpoc.common.utils.ObjectToExcelWriter;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.BeforeEach;
@@ -24,11 +26,30 @@ public class ObjectToExcelWriterTest {
     @Test
     @SneakyThrows
     void test_objArrayToExcel_expect_ok() throws IOException {
-        Account account = mockAccount();
-        List<Account> accountList = Collections.nCopies(20, account);
-//        excelFile.writeObjectsToExcelFile(accountList), path + "accounts-saltedge.xls");
-        excelFile.writeObjectToExcelFile(accountList.get(0), path + "accounts-saltedge.xls");
 
+
+        Account account = mockAccount();
+        Transaction trx = mockTransaction();
+//        List<Account> accountList = Collections.nCopies(20, account);
+//        excelFile.writeObjectsToExcelFile(accountList), path + "accounts-saltedge.xls");
+//        excelFile.writeObjectToExcelFile(accountList.get(0), path + "accounts-saltedge.xls");
+
+        ObjectToExcelWriter.writeObjectToExcelFile(trx, path + "test-trx.xls");
+    }
+
+    public static Transaction mockTransaction() {
+        return Transaction.builder()
+                .providerTransactionId("transactionId")
+                .creditorName("Jenny")
+                .debtorName("Lisa")
+                .creditor(Merchant.builder()
+                        .iban("NL1235ILPIZZA")
+                        .build())
+                .debtor(Merchant.builder()
+                        .iban("NL1235ILCOOKIES")
+                        .build())
+                .endToEndId("end_to_end_id")
+                .build();
     }
 
     public static Account mockAccount() {
